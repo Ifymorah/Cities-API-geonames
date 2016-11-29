74d3e45b459198c9c460e594e1c3a6dfbce19f73
Ify <ifeyinwal.anene@gmail.com>
2016-11-29 18:01:46 +0100

    corrected api for cities

--- apigeoname.js
+++ apigeoname.js
@@ -1,18 +1,17 @@
 var request = require('request');
 request ({
-	url: " http://api.geonames.org/citiesJSON/" // URL to go to
-	qs: {north: 44.1, south: -9.9, east: -22.4, west: 55.2, username: ifymorah };
+	url: " http://api.geonames.org/citiesJSON/", // URL to go to
+	qs: {north: 44.1, south: -9.9, east: -22.4, west: 55.2, username: 'ifymorah'},
 	method: "POST", //method specified
-	}
-	function (error, response, body) {
-		if (error) 
+	},
+	function(error, response, body) {
+		if(error) 
 		{
 			console.log(error);
 		}
 		else
 		{
-			console.log(response, StatusCode, body);
+			console.log(response.statusCode, body);
 		}
-	}
-
-})
+	
+});
